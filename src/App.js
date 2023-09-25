0c84b33f40c8b18f67b13d07cd79b31a6e969dd3
vwegba5522 <ogheneochukosalem@gmail.com>
2023-09-25 19:29:53 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -36,8 +36,30 @@ function reducer(state, { type, payload }) {
           currentOperand:null,
         }
       }
+
+      return {
+        ...state,
+        previousOperand: evaluate(state),
+        operation: payload.operation,
+        currentOperand: null
+      }
     case ACTIONS.CLEAR:
       return {};
+  }
+}
+
+function evaluate({ currentOperand, previousOperand, operation}) {
+  const prev = parseFloat(previousOperand)
+  const current = parseFloat(currentOperand)
+  if (isNaN(prev) || isNaN(current)) return ""
+  let computation = ""
+  switch (operation) {
+    case "+":
+      computation = prev + current
+      break
+      case "-":
+        computation = prev - current
+        break
   }
 }
 
